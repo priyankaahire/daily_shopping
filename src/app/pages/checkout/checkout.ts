9fa48aa242d3453edad3e21976614a2c85228eeb
priyankaahire <psahire01@gmail.com>
2020-05-25 01:18:11 +0530

    checkout page

--- src/app/pages/checkout/checkout.ts
+++ src/app/pages/checkout/checkout.ts
@@ -18,4 +18,17 @@ export class CheckoutPage implements OnInit {
   conform() {
     this.router.navigate(['/order-history']);
   }
+  addToCart() {
+    this.router.navigate(['/mycart'])
+  }
+  onSlideChanged(e) {
+    console.log('On slide change event');
+  }
+
+  onSlideChangeStart(event) {
+    /** isEnd true when slides reach at end slide */
+    event.target.isEnd().then(isEnd => {
+      console.log('End of slide', isEnd);
+    });
+  }
 }
