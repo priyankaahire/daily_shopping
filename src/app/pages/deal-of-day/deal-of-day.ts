951a2efb02bf642e65d507f141f41e055ca2b091
priyankaahire <psahire01@gmail.com>
2020-05-30 18:27:03 +0530

    deal of the day responsive css and on click on plus n minus button

--- src/app/pages/deal-of-day/deal-of-day.ts
+++ src/app/pages/deal-of-day/deal-of-day.ts
@@ -21,7 +21,7 @@ export class DealOfDayPage implements OnInit {
   back() {
     this.router.navigate(['/home']);
   }
-  gotoDetails(item) {
+  gotoDetails(item, event) {
     this.router.navigate(['/details']);
   }
   onSlideChangeStart(event) {
@@ -37,25 +37,29 @@ export class DealOfDayPage implements OnInit {
         title: "Palak 250g+Ladies Finger+Cucumber+Capsicum",
         imageUrl: "assets/imgs/vegitables.png",
         price:123.44,
-        desc:"3 combo pack fresh vegitables"
+        desc:"3 combo pack fresh vegitables",
+        count:2
       },
     {
       title: "Dove Bar Soap-Pack of 3",
       imageUrl: "assets/imgs/dove.png",
       price:400,
-      desc:"Cream Beauty Btahing Bar"
+      desc:"Cream Beauty Btahing Bar",
+      count:3
     },
     {
       title: "Body Care",
       imageUrl: "assets/imgs/personal_care.jpg",
       price:123.44,
-      desc:"3 combo pack fresh vegitables"
+      desc:"3 combo pack fresh vegitables",
+      count:4
     },
     {
       title: "Fruits",
       imageUrl: "assets/imgs/fruits.jpg",
       price:123.44,
-      desc:"3 combo pack fresh vegitables"
+      desc:"3 combo pack fresh vegitables",
+      count:3
     }
   ]
   }
@@ -102,6 +106,20 @@ export class DealOfDayPage implements OnInit {
         imageUrl: "assets/imgs/fruits.jpg"
       }
     ]
+  }
+
+  plus(idx, event:Event) {
+    event.stopPropagation();
+    if(this.items[idx].count != 10) {
+      this.items[idx].count += 1;
+    }
+  }
+
+  minus(idx,event:Event) {
+    event.stopPropagation();
+    if(this.items[idx].count != 0) {
+      this.items[idx].count -= 1;
+    }
   }
 
 
